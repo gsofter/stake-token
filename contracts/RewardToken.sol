22224695935c93eaffeb3563f5d91347c6803bd8
gsofter <real.kingstar@outlook.com>
2021-10-17 08:49:58 -0500

    update hardhat config

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract RewardToken is ERC20 {
     uint256 initSupply = 1000000 * 10**uint256(decimals());
 
-    constructor() public ERC20("gMold Reward Token", "gMRWD") {
+    constructor() ERC20("gMold Reward Token", "gMRWD") {
         _mint(msg.sender, initSupply);
     }
 }
--- contracts/StakeToken.sol
+++ contracts/StakeToken.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract StakeToken is ERC20 {
-    uint256 public initSupply = 1000000 * 10**uint256(decimals());
+    uint256 initSupply = 1000000 * 10**uint256(decimals());
 
-    constructor() public ERC20("gMold Staking Token", "gMST") {
+    constructor() ERC20("gMold Staking Token", "gMST") {
         _mint(msg.sender, initSupply);
     }
 }
